f967d2b5e891a0cfaeee41c54758dcf284b69534
Connor <csy7ay@virginia.edu>
2020-05-06 16:39:26 -0400

    switched debug back to true in testing

--- greeklink_core/settings.py
+++ greeklink_core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENV == 'testing':
-    DEBUG = False
+    DEBUG = True
 else:
     DEBUG = False
 
