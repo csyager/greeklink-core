4ef62fed5ded934cb5b951e7e1741b25cf014581
Connor <csy7ay@virginia.edu>
2020-05-09 12:45:51 -0400

    restyled the register form

--- core/forms.py
+++ core/forms.py
@@ -4,6 +4,16 @@ from django.contrib.auth.models import User
 from .models import *
 from django.forms import ModelForm
 from django.forms.widgets import TextInput
+from django.core.exceptions import ValidationError
+
+def getSettings():
+    settings = SiteSettings.objects.all()
+    if len(settings) == 0:
+        newsettings = SiteSettings()
+        newsettings.save()
+        settings = SiteSettings.objects.all()
+    settings = settings[0]
+    return settings
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -26,10 +36,32 @@ class LoginForm(AuthenticationForm):
         
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
-    first_name = forms.CharField(max_length=30, help_text='Required')
-    last_name = forms.CharField(max_length=30, help_text='Required')
-    verification_key = forms.CharField(max_length=10, help_text='Required')
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].label = "Password"
+        self.fields['password2'].label = "Confirm Password"
+
+    username=forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control rounded', 'placeholder': 'Username'}))
+    email = forms.EmailField(max_length=200, help_text='You will be asked to verify this email address in a moment.', 
+        widget=forms.EmailInput(attrs={'class': 'form-control rounded', 'placeholder': 'Email'}))
+    first_name = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control rounded', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control rounded', 'placeholder': 'Last Name'}))
+    verification_key = forms.CharField(max_length=10, help_text="This key should be provided by your site administrator",
+        widget=forms.TextInput(attrs={'class': 'form-control rounded', 'placeholder': 'Verification Key'}))
+    password1 = forms.CharField(max_length=30,
+        widget=forms.PasswordInput(attrs={'class': 'form-control rounded', 'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=30,
+        widget=forms.PasswordInput(attrs={'class': 'form-control rounded', 'placeholder': 'Confirm Password'}))
+
+    def clean_verification_key(self):
+        settings = getSettings()
+        verification_key = self.cleaned_data['verification_key']
+        if verification_key != settings.verification_key:
+            raise ValidationError("Verification key is incorrect")
+        return verification_key
 
     class Meta:
         model = User
