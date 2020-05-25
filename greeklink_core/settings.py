296821294d921370c8f44fe5e31b7536c8dd9a9f
Connor <csy7ay@virginia.edu>
2020-05-25 15:41:34 -0400

    setup media upload hosting on S3

--- greeklink_core/settings.py
+++ greeklink_core/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'crispy_forms',
-    'django_user_agents'
+    'django_user_agents',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -143,13 +144,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # for @login_required tag
 LOGIN_URL = '/login'
@@ -167,3 +162,23 @@ if ENV == 'testing':
     EMAIL_PORT = 587
 
 # else:  need to configure SMTP server wherever we host it
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if ENV == 'testing':
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# AWS S3 for media upload hosting
+else:
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    
