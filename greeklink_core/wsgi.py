0c73ee5d3f2712199fd1ca2d65f1826e44205ff9
Connor <csy7ay@virginia.edu>
2020-05-25 19:04:56 -0400

    cleaned up wsgi.py

--- greeklink_core/wsgi.py
+++ greeklink_core/wsgi.py
@@ -11,25 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-'''
-import time
-import traceback
-import signal
-import sys
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greeklink_core.settings')
-
-try:
-    application = get_wsgi_application()
-    print("WSGI without exception")
-except Exception:
-    print('handling WSGI exception')
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
-
-'''
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greeklink_core.settings')
 
 application = get_wsgi_application()
