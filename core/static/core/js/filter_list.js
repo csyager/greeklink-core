bc506d26546ab3d187abd60689bf4538cf3f9da4
Connor <csy7ay@virginia.edu>
2020-05-26 11:07:30 -0400

    added social list searching feature

---
+++ core/static/core/js/filter_list.js
@@ -0,0 +1,20 @@
+function filter_list() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("social_search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("list_table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            console.log(txtValue + ": " + txtValue.toUpperCase().indexOf(filter));
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].setAttribute('style', 'display:none !important');
+            }
+        }
+    }
+}
