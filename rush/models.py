91e4e25d22d9396e2b7d8d7ebfce5c2af724ea2f
Connor Yager <csy7ay@virginia.edu>
2020-07-03 09:01:06 -0400

    removed pylint problems from rush/models.py

--- rush/models.py
+++ rush/models.py
@@ -1,3 +1,5 @@
+""" database models for the rush app """
+
 from django.db import models
 from core.models import User, OrgEvent
 
@@ -10,6 +12,29 @@ YEAR_CHOICES = (
 
 # Create your models here.
 class Rushee(models.Model):
+    """ Represents a Rushee, or individual being considered for membership
+        name -- rushee's name
+        email -- email address
+        year -- class in school, i.e. Freshman, Sophomore, Junior, or Senior
+        major -- field of study
+        hometown -- hometown, stored as string
+        address -- current address, stored as string
+        phone_number -- phone number, stored as string, max 10 characters
+        endorsements -- Users who have endorsed this rushee
+        oppositions -- Users who have opposed this rushee
+        profile_picture -- picture taken at signin, stored as a file
+        voting_open -- boolean value, if true votes cast for this rushee will be tallied
+                       if false votes cast will not be tallied
+        y -- votes cast 'yes'
+        n -- votes cast 'no'
+        a -- votes cast 'abstain'
+        b -- votes cast 'blackball'
+        blackball_list -- list of Users who cast a blackball vote
+        round -- current voting round of this rushee
+        cut -- if 0, rushee has not been cut (removed from consideration),
+               otherwise represents which round the rushee was cut
+    """
+
     name = models.CharField(max_length=50)
     email = models.EmailField()
     year = models.IntegerField(choices=YEAR_CHOICES, default=1)
@@ -20,8 +45,9 @@ class Rushee(models.Model):
     endorsements = models.ManyToManyField(User, related_name="endorsed_rushees")
     oppositions = models.ManyToManyField(User, related_name="opposed_rushees")
 
-    profile_picture = models.ImageField(upload_to='media/rush/profile_pictures', default="", null=True, blank=True)
-    
+    profile_picture = models.ImageField(upload_to='media/rush/profile_pictures',
+                                        default="", null=True, blank=True)
+
     voting_open = models.BooleanField(default=False)
     y = models.IntegerField(default=0)
     n = models.IntegerField(default=0)
@@ -29,7 +55,7 @@ class Rushee(models.Model):
     b = models.IntegerField(default=0)
 
     blackball_list = models.ManyToManyField(User, related_name="blackballed_rushees")
-    
+
     round = models.IntegerField(default=1)
     cut = models.IntegerField(default=0)
 
@@ -37,6 +63,12 @@ class Rushee(models.Model):
         return self.name
 
 class Comment(models.Model):
+    """ a comment left by a User on a rushee
+        name -- name of User leaving the comment
+        body -- body text of the comment
+        rushee -- which Rushee the comment is being left on
+    """
+
     name = models.CharField(max_length=50)
     body = models.CharField(max_length=280)
     rushee = models.ForeignKey(Rushee, on_delete=models.CASCADE)
@@ -45,6 +77,16 @@ class Comment(models.Model):
         return "Comment " + str(self.pk)
 
 class RushEvent(OrgEvent):
+    """ OrgEvent (defined in core/models.py) representing a rush event
+        round -- which voting round this event falls into
+        attendance -- list of Rushees who attended the event
+        time -- time of the event
+        location -- location of the event
+        new_rushees_allowed -- boolean value, if true allows rushees to register as new
+                               rushees at signin, using the full form.  If false rushees
+                               just click their name from a list to signin without entering
+                               new information
+    """
     round = models.IntegerField(default=1)
     attendance = models.ManyToManyField(Rushee, blank=True)
     time = models.TimeField(default='12:00')
@@ -53,4 +95,3 @@ class RushEvent(OrgEvent):
 
     def __str__(self):
         return self.name
-
