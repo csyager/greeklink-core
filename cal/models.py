c83f997ce31858b3e139a8308fc58733b4f118c4
Connor Yager <csy7ay@virginia.edu>
2020-07-20 12:35:46 -0400

    added default permissions for calendar_admin

--- cal/models.py
+++ cal/models.py
@@ -12,6 +12,11 @@ RECURRENCE_CHOICES = [
 
 class ChapterEvent(OrgEvent):
     """ OrgEvent representing a miscellaneous chapter event.
+        recurring -- either weekly, monthly, or daily occurrence
+        start_date -- only set if recurring, represents start of recurrence
+        end_date -- only set if recurring, represents end of recurrence
+        is_recurrence -- true if event is created automatically by recurrence policy,
+                         otherwise false
     """
     recurring = models.CharField(choices=RECURRENCE_CHOICES, max_length=10, default=0)
     start_date = models.DateField(null=True)
