5a577a6d22386fe76dc475eadff3d71b31f6429f
Connor Yager <csy7ay@virginia.edu>
2020-07-22 10:08:06 -0400

    added placeholders for time and date fields for safari users

--- cal/forms.py
+++ cal/forms.py
@@ -8,15 +8,15 @@ class ChapterEventForm(ModelForm):
     name = forms.CharField(max_length=50, label='Name',
         widget=forms.TextInput(attrs={'class': 'form-control rounded'}))
     date = forms.DateField(initial=datetime.date.today, label='Date',
-        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control rounded'}))
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control rounded', 'placeholder': 'YYYY-mm-dd'}))
     time = forms.TimeField(label="Time",
-        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'form-control rounded'}))
+        widget=forms.TimeInput(attrs={'type': 'time', 'class': 'form-control rounded', 'placeholder': "HH:mm in 24 hour time"}))
     location = forms.CharField(max_length=50, label='Location',
         widget=forms.TextInput(attrs={'class': 'form-control rounded'}))
     recurring = forms.ChoiceField(choices=RECURRENCE_CHOICES, label="Recurring",
         widget=forms.Select(attrs={'id': 'recurring_selection', 'class': 'form-control rounded'}))
     end_date = forms.DateField(label='End Date', required=False, initial=datetime.date.today,
-        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control rounded'}))
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control rounded', 'placeholder': 'YYYY-mm-dd'}))
 
     def clean(self):
         recurring = self.cleaned_data.get('recurring')
