3e180abc8fdc7a8a81b24f4251f7f5c11160acc1
Connor Yager <csy7ay@virginia.edu>
2020-08-24 17:17:19 -0400

    added a few missing things to admin module

--- rush/admin.py
+++ rush/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Rushee, RushEvent
+from .models import Rushee, RushEvent, Comment
 
 # Register your models here.
 admin.site.register(Rushee)
 admin.site.register(RushEvent)
+admin.site.register(Comment)
