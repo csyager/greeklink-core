3b7429bc3726b4a25286700b8dbafc23a2caee92
Connor Yager <csy7ay@virginia.edu>
2020-08-26 16:30:22 -0400

    removed urlpatterns add on in core.urls

--- core/urls.py
+++ core/urls.py
@@ -49,5 +49,5 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG is True:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
