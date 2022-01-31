c9c2970ce25490b8cee40957fefb00eb1b03a7ed
Connor Yager <csy7ay@virginia.edu>
2022-01-31 11:32:39 -0500

    remove xml coverage from standard coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,7 +5,6 @@ coverage erase
 # run new coverage report
 coverage run manage.py test
 coverage html
-coverage xml
 # open results in web browser
 python -m webbrowser htmlcov/index.html
 open htmlcov/index.html
