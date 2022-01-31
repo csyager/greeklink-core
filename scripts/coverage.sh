365386898e11d5e1829e6ce576d9ce930e723c4c
Connor Yager <csy7ay@virginia.edu>
2022-01-30 19:07:58 -0500

    trying to setup sonarqube coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -5,6 +5,7 @@ coverage erase
 # run new coverage report
 coverage run manage.py test
 coverage html
+coverage xml
 # open results in web browser
 python -m webbrowser htmlcov/index.html
 open htmlcov/index.html
